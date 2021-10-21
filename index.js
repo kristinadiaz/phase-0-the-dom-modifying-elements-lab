00eb126d0fae0f987df2d2f48daa1f6ec800429f
Kristina Diaz <kristina.diaz84@gmail.com>
2021-10-21 13:01:09 -0600

    2nd attempt, completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const main = document.querySelector("#main");
-
 main.remove();
 
 const newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory");
-newHeader.innerHTML = "Kristina is the champion";
+newHeader.textContent = "Kristina is the champion";
 document.body.append(newHeader);
-
-
-
-
-
-
-
-
